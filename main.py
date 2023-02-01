50a81f2e29b7842ec8b2d6cae7c032f4a7d41413
Lemon <me@lemonbro.ru>
2023-02-01 23:19:47 +0500

    fix button logic

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import time
-
 from PyQt5.QtWidgets import QWidget, QPushButton, QApplication, QLabel, QCheckBox
 from PyQt5.QtGui import QPainter, QPen, QColor
 from PyQt5.QtCore import Qt, QObject, QThread, pyqtSignal
@@ -10,6 +9,18 @@ import sys
 from os import path, mkdir
 import config
 from datetime import datetime as dt
+
+global filename
+filename = f'test_{dt.now().strftime("%Y-%m-%d_%H-%M-%S")}.csv'
+
+global piston_master
+piston_master = {
+        'p1': 'on',
+        'p2': 'on',
+        'p3': 'on',
+        'p4': 'on',
+        'p5': 'on'
+    }
 
 global q
 q = queue.LifoQueue()
@@ -30,9 +41,8 @@ class GetData(QThread):
     scale_5 = pyqtSignal(str)
 
     def run(self):
-        filename = f'test_{dt.now().strftime("%Y-%m-%d_%H-%M-%S")}.csv'
         with open(filename, 'a') as f:
-            f.write("date;p1;p2;p3;p4;p5")
+            f.write("date;p1;p2;p3;p4;p5\n")
         while True:
             try:
                 line = self.serial.readline()
@@ -51,38 +61,102 @@ class GetData(QThread):
 
 
 class CheckData(QThread):
-    def __init__(self):
+    def __init__(self, serial):
         QtCore.QThread.__init__(self)
+        self.serial = serial
 
     running = False
+    p1 = pyqtSignal()
+    p2 = pyqtSignal()
+    p3 = pyqtSignal()
+    p4 = pyqtSignal()
+    p5 = pyqtSignal()
+
     def run(self):
-        with q.mutex:
-            q.queue.clear()
-        time.sleep(config.SLEEP_AFTER_START)
-        if not q.empty():
-            data = q.get_nowait()
-            print(data)
-            print(config.MIN_WEIGHT)
-            print(float(data[0]))
-            print(float(data[0]) < float(config.MIN_WEIGHT))
-            print('123')
-            self.do_btn_1_2_on_off()
+        while self.running:
+            with q.mutex:
+                q.queue.clear()
+            if piston_master['p1'] == 'on':
+                self.serial.write('1'.encode('ascii'))
+            if piston_master['p2'] == 'on':
+                self.serial.write('2'.encode('ascii'))
+            if piston_master['p3'] == 'on':
+                self.serial.write('3'.encode('ascii'))
+            if piston_master['p4'] == 'on':
+                self.serial.write('4'.encode('ascii'))
+            if piston_master['p5'] == 'on':
+                self.serial.write('5'.encode('ascii'))
+            time.sleep(config.SLEEP_AFTER_START)
+            if not q.empty():
+                data = q.get_nowait()
+                print(f'piston_master: {piston_master}')
+                print(f'data: {data}')
+                print(f'min: {config.MIN_WEIGHT}')
+                if piston_master['p1'] == 'on':
+                    if float(data[0]) < float(config.MIN_WEIGHT):
+                        piston_master['p1'] = 'off'
+                        self.p1.emit()
+                elif piston_master['p1'] == 'off':
+                    if float(data[0]) > float(config.MIN_WEIGHT):
+                        piston_master['p1'] = 'on'
+                        self.p1.emit()
+
+                if piston_master['p2'] == 'on':
+                    if float(data[1]) < float(config.MIN_WEIGHT):
+                        piston_master['p2'] = 'off'
+                        self.p2.emit()
+                elif piston_master['p2'] == 'off':
+                    if float(data[1]) > float(config.MIN_WEIGHT):
+                        piston_master['p2'] = 'on'
+                        self.p2.emit()
+
+                if piston_master['p3'] == 'on':
+                    if float(data[2]) < float(config.MIN_WEIGHT):
+                        piston_master['p3'] = 'off'
+                        self.p3.emit()
+                elif piston_master['p3'] == 'off':
+                    if float(data[2]) > float(config.MIN_WEIGHT):
+                        piston_master['p3'] = 'on'
+                        self.p3.emit()
+
+                if piston_master['p4'] == 'on':
+                    if float(data[3]) < float(config.MIN_WEIGHT):
+                        piston_master['p4'] = 'off'
+                        self.p4.emit()
+                elif piston_master['p4'] == 'off':
+                    if float(data[3]) > float(config.MIN_WEIGHT):
+                        piston_master['p4'] = 'on'
+                        self.p4.emit()
+
+                if piston_master['p5'] == 'on':
+                    if float(data[4]) < float(config.MIN_WEIGHT):
+                        piston_master['p5'] = 'off'
+                        self.p5.emit()
+                elif piston_master['p5'] == 'off':
+                    if float(data[4]) > float(config.MIN_WEIGHT):
+                        piston_master['p5'] = 'on'
+                        self.p5.emit()
+
+                if piston_master['p1'] == 'on':
+                    self.serial.write('1'.encode('ascii'))
+                if piston_master['p2'] == 'on':
+                    self.serial.write('2'.encode('ascii'))
+                if piston_master['p3'] == 'on':
+                    self.serial.write('3'.encode('ascii'))
+                if piston_master['p4'] == 'on':
+                    self.serial.write('4'.encode('ascii'))
+                if piston_master['p5'] == 'on':
+                    self.serial.write('5'.encode('ascii'))
+            time.sleep(config.SLEEP_AFTER_CHECK)
 
 
 class Main(QWidget):
-    piston_state = {
-        'p1': 'off',
-        'p2': 'off',
-        'p3': 'off',
-        'p4': 'off',
-        'p5': 'off'
-    }
-    piston_hand = {
-        'p1': 'off',
-        'p2': 'off',
-        'p3': 'off',
-        'p4': 'off',
-        'p5': 'off'
+    piston_color = {
+        'p1': 'g',
+        'p2': 'g',
+        'p3': 'g',
+        'p4': 'g',
+        'p5': 'g'
     }
 
     def __init__(self):
@@ -98,24 +172,29 @@ class Main(QWidget):
         self.label_4_1 = QLabel('Поршень 4', self)
         self.label_5_1 = QLabel('Поршень 5', self)
         self.label_0_2 = QLabel('Включен?', self)
-        self.btn_1_2 = QCheckBox('Выключен', self)
-        self.btn_2_2 = QCheckBox('Выключен', self)
-        self.btn_3_2 = QCheckBox('Выключен', self)
-        self.btn_4_2 = QCheckBox('Выключен', self)
-        self.btn_5_2 = QCheckBox('Выключен', self)
+        self.btn_1_2 = QCheckBox('Включен', self)
+        self.btn_2_2 = QCheckBox('Включен', self)
+        self.btn_3_2 = QCheckBox('Включен', self)
+        self.btn_4_2 = QCheckBox('Включен', self)
+        self.btn_5_2 = QCheckBox('Включен', self)
         self.label_0_3 = QLabel('Давление', self)
-        self.label_1_3 = QLabel('0.00', self)
-        self.label_2_3 = QLabel('0.00', self)
-        self.label_3_3 = QLabel('0.00', self)
-        self.label_4_3 = QLabel('0.00', self)
-        self.label_5_3 = QLabel('0.00', self)
+        self.label_1_3 = QLabel('0,00', self)
+        self.label_2_3 = QLabel('0,00', self)
+        self.label_3_3 = QLabel('0,00', self)
+        self.label_4_3 = QLabel('0,00', self)
+        self.label_5_3 = QLabel('0,00', self)
         self.thread_get = GetData(self.serial)
-        self.thread_check = CheckData()
+        self.thread_check = CheckData(self.serial)
         self.thread_get.scale_1.connect(self.label_1_3.setText)
         self.thread_get.scale_2.connect(self.label_2_3.setText)
         self.thread_get.scale_3.connect(self.label_3_3.setText)
         self.thread_get.scale_4.connect(self.label_4_3.setText)
         self.thread_get.scale_5.connect(self.label_5_3.setText)
+        self.thread_check.p1.connect(self.do_btn_1_2_color)
+        self.thread_check.p2.connect(self.do_btn_2_2_color)
+        self.thread_check.p3.connect(self.do_btn_3_2_color)
+        self.thread_check.p4.connect(self.do_btn_4_2_color)
+        self.thread_check.p5.connect(self.do_btn_5_2_color)
 
         self.initUI()
 
@@ -140,7 +219,7 @@ class Main(QWidget):
         self.label_1_1.resize(150, 50)
         self.label_1_1.move(150, 100)
         self.label_1_1.setAlignment(Qt.AlignCenter)
-        self.label_1_1.setStyleSheet("QLabel {background-color: yellow;}")
+        self.label_1_1.setStyleSheet("QLabel {background-color: green;}")
 
         self.btn_1_2.resize(self.btn_1_2.sizeHint())
         self.btn_1_2.resize(150, 50)
@@ -150,7 +229,7 @@ class Main(QWidget):
         self.label_2_1.resize(150, 50)
         self.label_2_1.move(300, 100)
         self.label_2_1.setAlignment(Qt.AlignCenter)
-        self.label_2_1.setStyleSheet("QLabel {background-color: yellow;}")
+        self.label_2_1.setStyleSheet("QLabel {background-color: green;}")
 
         self.btn_2_2.resize(self.btn_2_2.sizeHint())
         self.btn_2_2.resize(150, 50)
@@ -160,7 +239,7 @@ class Main(QWidget):
         self.label_3_1.resize(150, 50)
         self.label_3_1.move(450, 100)
         self.label_3_1.setAlignment(Qt.AlignCenter)
-        self.label_3_1.setStyleSheet("QLabel {background-color: yellow;}")
+        self.label_3_1.setStyleSheet("QLabel {background-color: green;}")
 
         self.btn_3_2.resize(self.btn_3_2.sizeHint())
         self.btn_3_2.resize(150, 50)
@@ -170,7 +249,7 @@ class Main(QWidget):
         self.label_4_1.resize(150, 50)
         self.label_4_1.move(600, 100)
         self.label_4_1.setAlignment(Qt.AlignCenter)
-        self.label_4_1.setStyleSheet("QLabel {background-color: yellow;}")
+        self.label_4_1.setStyleSheet("QLabel {background-color: green;}")
 
         self.btn_4_2.resize(self.btn_4_2.sizeHint())
         self.btn_4_2.resize(150, 50)
@@ -180,7 +259,7 @@ class Main(QWidget):
         self.label_5_1.resize(150, 50)
         self.label_5_1.move(750, 100)
         self.label_5_1.setAlignment(Qt.AlignCenter)
-        self.label_5_1.setStyleSheet("QLabel {background-color: yellow;}")
+        self.label_5_1.setStyleSheet("QLabel {background-color: green;}")
 
         self.btn_5_2.resize(self.btn_5_2.sizeHint())
         self.btn_5_2.resize(150, 50)
@@ -210,6 +289,11 @@ class Main(QWidget):
         self.setGeometry(0, 0, 1280, 720)
         self.setWindowTitle('TESTER')
         self.show()
+        self.btn_1_2.toggle()
+        self.btn_2_2.toggle()
+        self.btn_3_2.toggle()
+        self.btn_4_2.toggle()
+        self.btn_5_2.toggle()
         self.thread_get.start()
 
     def do_start_stop(self):
@@ -217,74 +301,136 @@ class Main(QWidget):
         if self.start_stop_btn.text() == 'START':
             self.start_stop_btn.setText('STOP')
             self.thread_check.start()
+            self.thread_check.running = True
         elif self.start_stop_btn.text() == 'STOP':
             self.start_stop_btn.setText('START')
+            self.thread_check.running = False
             self.thread_check.quit()
-        self.do_btn_1_2_on_off()
-        self.btn_1_2.toggle()
-        self.do_btn_2_2_on_off()
-        self.btn_2_2.toggle()
-        self.do_btn_3_2_on_off()
-        self.btn_3_2.toggle()
-        self.do_btn_4_2_on_off()
-        self.btn_4_2.toggle()
-        self.do_btn_5_2_on_off()
-        self.btn_5_2.toggle()
+            if piston_master['p1'] == 'on':
+                self.serial.write('1'.encode('ascii'))
+            if piston_master['p2'] == 'on':
+                self.serial.write('2'.encode('ascii'))
+            if piston_master['p3'] == 'on':
+                self.serial.write('3'.encode('ascii'))
+            if piston_master['p4'] == 'on':
+                self.serial.write('4'.encode('ascii'))
+            if piston_master['p5'] == 'on':
+                self.serial.write('5'.encode('ascii'))
+
 
     def do_btn_1_2_on_off(self):
-        if self.piston_state['p1'] == 'on':
+        if self.btn_1_2.text() == 'Включен':
             self.btn_1_2.setText('Выключен')
-            self.piston_state['p1'] = 'off'
-            self.label_1_1.setStyleSheet("QLabel {background-color: yellow;}")
-        elif self.piston_state['p1'] == 'off':
+            piston_master['p1'] = 'off'
+            # self.label_1_1.setStyleSheet("QLabel {background-color: yellow;}")
+        elif self.btn_1_2.text() == 'Выключен':
             self.btn_1_2.setText('Включен')
-            self.piston_state['p1'] = 'on'
+            piston_master['p1'] = 'on'
+            # self.label_1_1.setStyleSheet("QLabel {background-color: green;}")
+        if piston_master['p1'] == 'off' and self.piston_color['p1'] == 'y':
             self.label_1_1.setStyleSheet("QLabel {background-color: green;}")
-        self.serial.write('1'.encode('ascii'))
+        # self.serial.write('1'.encode('ascii'))
+        # self.btn_1_2.toggle()
 
     def do_btn_2_2_on_off(self):
-        if self.piston_state['p2'] == 'on':
+        if self.btn_2_2.text() == 'Включен':
             self.btn_2_2.setText('Выключен')
-            self.piston_state['p2'] = 'off'
-            self.label_2_1.setStyleSheet("QLabel {background-color: yellow;}")
-        elif self.piston_state['p2'] == 'off':
+            piston_master['p2'] = 'off'
+            # self.label_2_1.setStyleSheet("QLabel {background-color: yellow;}")
+        elif self.btn_2_2.text() == 'Выключен':
             self.btn_2_2.setText('Включен')
-            self.piston_state['p2'] = 'on'
+            piston_master['p2'] = 'on'
+            # self.label_2_1.setStyleSheet("QLabel {background-color: green;}")
+        if piston_master['p2'] == 'off' and self.piston_color['p2'] == 'y':
             self.label_2_1.setStyleSheet("QLabel {background-color: green;}")
-        self.serial.write('2'.encode('ascii'))
+            self.piston_color['p2'] = 'g'
+        # self.serial.write('2'.encode('ascii'))
+        # self.btn_2_2.toggle()
 
     def do_btn_3_2_on_off(self):
-        if self.piston_state['p3'] == 'on':
+        if self.btn_3_2.text() == 'Включен':
             self.btn_3_2.setText('Выключен')
-            self.piston_state['p3'] = 'off'
-            self.label_3_1.setStyleSheet("QLabel {background-color: yellow;}")
-        elif self.piston_state['p3'] == 'off':
+            piston_master['p3'] = 'off'
+            # self.label_3_1.setStyleSheet("QLabel {background-color: yellow;}")
+        elif self.btn_3_2.text() == 'Выключен':
             self.btn_3_2.setText('Включен')
-            self.piston_state['p3'] = 'on'
+            piston_master['p3'] = 'on'
+            # self.label_3_1.setStyleSheet("QLabel {background-color: green;}")
+        if piston_master['p3'] == 'off' and self.piston_color['p3'] == 'y':
             self.label_3_1.setStyleSheet("QLabel {background-color: green;}")
-        self.serial.write('3'.encode('ascii'))
+            self.piston_color['p3'] = 'g'
+        # self.serial.write('3'.encode('ascii'))
+        # self.btn_3_2.toggle()
 
     def do_btn_4_2_on_off(self):
-        if self.piston_state['p4'] == 'on':
+        if self.btn_4_2.text() == 'Включен':
             self.btn_4_2.setText('Выключен')
-            self.piston_state['p4'] = 'off'
-            self.label_4_1.setStyleSheet("QLabel {background-color: yellow;}")
-        elif self.piston_state['p4'] == 'off':
+            piston_master['p4'] = 'off'
+            # self.label_4_1.setStyleSheet("QLabel {background-color: yellow;}")
+        elif self.btn_4_2.text() == 'Выключен':
             self.btn_4_2.setText('Включен')
-            self.piston_state['p4'] = 'on'
+            piston_master['p4'] = 'on'
+            # self.label_4_1.setStyleSheet("QLabel {background-color: green;}")
+        if piston_master['p4'] == 'off' and self.piston_color['p4'] == 'y':
             self.label_4_1.setStyleSheet("QLabel {background-color: green;}")
-        self.serial.write('4'.encode('ascii'))
+            self.piston_color['p4'] = 'g'
+        # self.serial.write('4'.encode('ascii'))
+        # self.btn_4_2.toggle()
 
     def do_btn_5_2_on_off(self):
-        if self.piston_state['p5'] == 'on':
+        if self.btn_5_2.text() == 'Включен':
             self.btn_5_2.setText('Выключен')
-            self.piston_state['p5'] = 'off'
-            self.label_5_1.setStyleSheet("QLabel {background-color: yellow;}")
-        elif self.piston_state['p5'] == 'off':
+            piston_master['p5'] = 'off'
+            # self.label_5_1.setStyleSheet("QLabel {background-color: yellow;}")
+        elif self.btn_5_2.text() == 'Выключен':
             self.btn_5_2.setText('Включен')
-            self.piston_state['p5'] = 'on'
+            piston_master['p5'] = 'on'
+            # self.label_5_1.setStyleSheet("QLabel {background-color: green;}")
+        if piston_master['p5'] == 'off' and self.piston_color['p5'] == 'y':
             self.label_5_1.setStyleSheet("QLabel {background-color: green;}")
-        self.serial.write('5'.encode('ascii'))
+            self.piston_color['p5'] = 'g'
+        # self.serial.write('5'.encode('ascii'))
+        # self.btn_5_2.toggle()
+
+    def do_btn_1_2_color(self):
+        if self.piston_color['p1'] == 'g':
+            self.label_1_1.setStyleSheet("QLabel {background-color: yellow;}")
+            self.piston_color['p1'] = 'y'
+        elif self.piston_color['p1'] == 'y':
+            self.label_1_1.setStyleSheet("QLabel {background-color: green;}")
+            self.piston_color['p1'] = 'g'
+
+    def do_btn_2_2_color(self):
+        if self.piston_color['p2'] == 'g':
+            self.label_2_1.setStyleSheet("QLabel {background-color: yellow;}")
+            self.piston_color['p2'] = 'y'
+        elif self.piston_color['p2'] == 'y':
+            self.label_2_1.setStyleSheet("QLabel {background-color: green;}")
+            self.piston_color['p2'] = 'g'
+
+    def do_btn_3_2_color(self):
+        if self.piston_color['p3'] == 'g':
+            self.label_3_1.setStyleSheet("QLabel {background-color: yellow;}")
+            self.piston_color['p3'] = 'y'
+        elif self.piston_color['p3'] == 'y':
+            self.label_3_1.setStyleSheet("QLabel {background-color: green;}")
+            self.piston_color['p3'] = 'g'
+
+    def do_btn_4_2_color(self):
+        if self.piston_color['p4'] == 'g':
+            self.label_4_1.setStyleSheet("QLabel {background-color: yellow;}")
+            self.piston_color['p4'] = 'y'
+        elif self.piston_color['p4'] == 'y':
+            self.label_4_1.setStyleSheet("QLabel {background-color: green;}")
+            self.piston_color['p4'] = 'g'
+
+    def do_btn_5_2_color(self):
+        if self.piston_color['p5'] == 'g':
+            self.label_5_1.setStyleSheet("QLabel {background-color: yellow;}")
+            self.piston_color['p5'] = 'y'
+        elif self.piston_color['p5'] == 'y':
+            self.label_5_1.setStyleSheet("QLabel {background-color: green;}")
+            self.piston_color['p5'] = 'g'
 
 
 if __name__ == '__main__':
